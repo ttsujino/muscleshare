8f5e7cbef92a55e86021046b6f1ea86b83364d25
ttsujino <ttsujino@ridge-i.com>
2024-09-01 21:35:33 +0900

    import分の簡素化

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Container, Grid, Paper, Typography, Box } from '@mui/material';
 
 const posts = [
   {
