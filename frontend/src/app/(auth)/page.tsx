8a2e67129787ac8f45bf5fffb7fea0b81c38986e
ttsujino <ttsujino@ridge-i.com>
2024-10-01 07:54:05 +0900

    サインインページにログアウトが表示されないよう変更

--- frontend/src/app/(auth)/page.tsx
+++ frontend/src/app/(auth)/page.tsx
@@ -1,34 +1,12 @@
 "use client";
 import styles from "./page.module.css";
-import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Login() {
     return (
         <main>
             <div className={styles.container}>
-                <div>
-                    <div>
-                        <SignInComponent />
-                    </div>
-                </div>
+                <a href="/api/auth/login">Login</a>
             </div>
         </main>
     );
-}
-
-function SignInComponent() {
-    const { user, error, isLoading } = useUser();
-  
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>{error.message}</div>;
-  
-    if (user) {
-        return (
-            <div>
-                Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-            </div>
-        );
-    }
-
-    return <a href="/api/auth/login">Login</a>;
 }
