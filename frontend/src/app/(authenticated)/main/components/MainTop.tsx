6ebc6a09b46bcfbb7445d27c899c83607e175fb7
ttsujino <ttsujino@ridge-i.com>
2024-10-02 21:33:59 +0900

    自己紹介欄を追加して要素を整列

--- frontend/src/app/(authenticated)/main/components/MainTop.tsx
+++ frontend/src/app/(authenticated)/main/components/MainTop.tsx
@@ -3,12 +3,18 @@ import Image from 'next/image';
 
 const MainTop: React.FC<{ user_name?: string }> = ({ user_name }) => {
     return (
-        <div className={styles.top}>
-            <div className={styles.user_name}>
-                <p>{user_name}</p>
-            </div>
+        <div className={styles.top_container}>
             <div>
-                <Image src="/sample/black.png" alt="main" width={150} height={150} className={styles.icon} />
+                <div className={styles.user_name}>
+                    <p>{user_name}</p>
+                </div>
+                <div>
+                    <Image src="/sample/black.png" alt="main" width={150} height={150} className={styles.icon} />
+                </div>
+            </div>
+            <div className={styles.introduction}>
+                <p>自己紹介</p>
+                <p>自己紹介文が入ります。</p>
             </div>
         </div>
     );
