431d18f686d337af0450780ff9b5578303dfe3f9
ttsujino <ttsujino@ridge-i.com>
2024-10-09 08:45:20 +0900

    画像が保存されるように修正

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -6,8 +6,9 @@ use axum::{
 use crate::repositories::PostRepository;
 use std::sync::Arc;
 use uuid::Uuid;
-use tokio::io::AsyncWriteExt;
 use tokio::fs;
+use tokio::fs::metadata;
+use tokio::io::AsyncWriteExt;
 
 pub async fn create_post<T: PostRepository>(
     Extension(repository): Extension<Arc<T>>,
@@ -16,30 +17,31 @@ pub async fn create_post<T: PostRepository>(
 ) -> Result<impl IntoResponse, StatusCode> {
 
     let mut content = String::new();
-    let image_id = Uuid::new_v4().to_string();
-    // // フォルダ作成
-    // fs::create_dir("/imgs").await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let image_id = Uuid::new_v4();
+    if metadata("./imgs").await.is_err() {
+        fs::create_dir("./imgs").await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    }
 
     while let Some(field) = multipart.next_field().await.unwrap() {
-        let field_name = field.name().unwrap().to_string();
-        match field_name.as_str() {
+        let param_name = field.name().unwrap();
+        match param_name {
             "content" => {
                 content = field.text().await.unwrap();
             }
             "image" => {
                 let image = field.bytes().await.unwrap();
-                let image_fname = format!("{}.jpg", Uuid::new_v4());
+                let image_fname = format!("{}.jpg", image_id);
                 let mut file = fs::File::create(format!("./imgs/{}", image_fname)).await.unwrap();
                 file.write_all(&image).await.unwrap();
             }
             _ => {
-                tracing::warn!("Unexpected field: {}", field_name);
+                tracing::warn!("Unexpected field: {}", param_name);
             }
         }   
     }
 
     let post = repository
-        .create(user_id, content, Uuid::parse_str(&image_id).unwrap())
+        .create(user_id, content, image_id)
         .await
         .or(Err(StatusCode::NOT_FOUND))?;
 
@@ -106,7 +108,7 @@ mod tests {
     };
     use crate::repositories::PostRepositoryForDb;
     use axum_test::TestServer;
-    // use serde_json::json;
+    use serde_json::json;
 
     fn create_app<T: PostRepository>(repository: T) -> Router {
         Router::new()
@@ -115,23 +117,30 @@ mod tests {
         .layer(Extension(Arc::new(repository)))
     }
 
+    async fn setup_test<T: PostRepository>(pool: PgPool) -> TestServer {
+        let repository = PostRepositoryForDb::new(pool);
+        let app = create_app(repository);
+        TestServer::new(app).unwrap()
+    }
+
     #[sqlx::test]
     async fn test_create_post(pool: PgPool) {
-        let repository = PostRepositoryForDb::new(pool);
+        let server = setup_test::<PostRepositoryForDb>(pool).await;
 
-        let app = create_app(repository);
+        let response = server.post("/post/new/1")
+            .json(&json!({
+                "content": "test",
+                "image": "@/Users/t0721/Pictures/black.png"
+            }))
+            .await;
+        response.assert_status_ok();
+    }
 
-        let server = TestServer::new(app).unwrap();
+    #[sqlx::test]
+    async fn test_get_all_posts(pool: PgPool) {
+        let server = setup_test::<PostRepositoryForDb>(pool).await;
 
         let response = server.get("/posts").await;
         response.assert_status_ok();
-
-        // let response = server.post("/post/new/1")
-        //     .json(&json!({
-        //         "content": "test",
-        //         "image": "@/Users/t0721/Pictures/black.png"
-        //     }))
-        //     .await;
-        // response.assert_status_ok();
     }
 }
