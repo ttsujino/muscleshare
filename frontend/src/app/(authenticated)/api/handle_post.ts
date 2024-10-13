42b1f5c41ec6747e37bdc43108e0eead27c9d73f
ttsujino <ttsujino@ridge-i.com>
2024-10-13 13:11:30 +0900

    update: 画像を取得する処理を関数化

---
+++ frontend/src/app/(authenticated)/api/handle_post.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const fetchPosts = async () => {
+    try {
+        const response = await axios.get("http://localhost:3000/posts");
+        const data = await response.data;
+        const updatedPosts = data.map((post: any) => ({
+        ...post,
+        image: `http://localhost:3000/image/${post.image_id}`
+        }));
+    
+        return updatedPosts;
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+}
+
+export const fetchPost = async (image_id: string) => {
+    try {
+        const response = await axios.get(`${process.env.BACKEND_API_URL}/post/${image_id}`);
+        const data = await response.data;
+        const updatedPost = {
+            ...data,
+            image: `http://localhost:3000/image/${data.image_id}`
+        };
+        return updatedPost;
+    } catch (error) {
+        console.error('Error fetching post:', error);
+    }
+}
