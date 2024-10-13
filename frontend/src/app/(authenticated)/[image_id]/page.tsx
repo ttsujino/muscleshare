350c8116e5338550effde3f044d89385af25f068
ttsujino <ttsujino@ridge-i.com>
2024-10-13 12:48:43 +0900

    bugfix: 不要ファイル削除

--- frontend/src/app/(authenticated)/[image_id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-const ImageDisplay: React.FC<{ imageUrl: string }> = ({ imageUrl }) => {
-    return (
-        <div>
-            <Image src={imageUrl} alt="表示する画像" width={500} height={500} />
-        </div>
-    );
-};
