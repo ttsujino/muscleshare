14111bd44621c1c1c9e07c988b9d61f826adb42a
ttsujino <ttsujino@ridge-i.com>
2024-10-13 13:23:00 +0900

    update: 型を定義

--- frontend/src/app/(authenticated)/api/handle_post.ts
+++ frontend/src/app/(authenticated)/api/handle_post.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
-export const fetchPosts = async () => {
+interface Post {
+    id: string;
+    user_id: string;
+    content: string;
+    image_id: string;
+}
+
+export const fetchPosts = async (): Promise<Post[] | undefined> => {
     try {
         const response = await axios.get("http://localhost:3000/posts");
-        const data = await response.data;
-        const updatedPosts = data.map((post: any) => ({
-        ...post,
-        image: `http://localhost:3000/image/${post.image_id}`
+        const data: Post[] = await response.data;
+        const updatedPosts = data.map((post) => ({
+            ...post,
+            image: `http://localhost:3000/image/${post.image_id}`
         }));
     
         return updatedPosts;
@@ -15,10 +22,10 @@ export const fetchPosts = async () => {
     }
 }
 
-export const fetchPost = async (image_id: string) => {
+export const fetchPost = async (image_id: string): Promise<Post | undefined> => {
     try {
         const response = await axios.get(`${process.env.BACKEND_API_URL}/post/${image_id}`);
-        const data = await response.data;
+        const data: Post = await response.data;
         const updatedPost = {
             ...data,
             image: `http://localhost:3000/image/${data.image_id}`
