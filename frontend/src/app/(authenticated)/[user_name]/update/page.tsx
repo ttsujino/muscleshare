f9a651fa6a7d6184ba1580c15e6b56be7ca743c1
ttsujino <ttsujino@ridge-i.com>
2024-10-19 20:59:41 +0900

    feat: デフォルト画像が表示されるよう更新

--- frontend/src/app/(authenticated)/[user_name]/update/page.tsx
+++ frontend/src/app/(authenticated)/[user_name]/update/page.tsx
@@ -1,30 +1,64 @@
+'use client';
+import { useState } from "react";
 import styles from "./update.module.css";
+import Image from 'next/image';
 
 export default function UpdatePage() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setSelectedImage(e.target?.result as string);
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
 
   return (
     <form action="" method="get" className={styles['form-example']}>
       <div className={styles['form-example']}>
-        <label htmlFor="user_id">user id: </label>
+        <label htmlFor="user_id">user id</label>
         <input
           type="text"
           name="user_id"
           id="user_id"
-          defaultValue="asdf"
           required
+          maxLength={20}
         />
       </div>
       <div className={styles['form-example']}>
-        <label htmlFor="bio">bio: </label>
-        <textarea name="bio" id="bio" rows={4} cols={40}></textarea>
+        <label htmlFor="bio">bio</label>
+        <textarea
+          name="bio"
+          id="bio"
+          rows={4}
+          cols={40}>
+        </textarea>
       </div>
       <div className={styles['form-example']}>
-        <label htmlFor="icon">bio: </label>
-        <input type="file" name="icon" id="icon" required />
+        <label htmlFor="icon">icon</label>
+        <input
+          type="file"
+          name="icon"
+          id="icon"
+          onChange={handleImageChange}
+        />
+        <div style={{ marginTop: '20px' }}>
+          <Image
+            src={selectedImage || '/default_icon.png'}
+            alt="Selected"
+            width={500}
+            height={500} style={{ maxWidth: '100%', height: 'auto' }}
+          />
+        </div>
       </div>
       <div className={styles['form-example']}>
-        <input type="submit" value="Update Profile!!" />
+        <input
+          type="submit"
+          value="Update Profile!!"
+        />
       </div>
     </form>
   );
