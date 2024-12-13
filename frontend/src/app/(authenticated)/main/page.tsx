a8680ab8c55afa4a432e662b30312098165f1c5b
ttsujino <ttsujino@ridge-i.com>
2024-12-14 08:49:30 +0900

    プロフィール修正後にmainページのリダイレクト先に反映されないバグを修正

--- frontend/src/app/(authenticated)/main/page.tsx
+++ frontend/src/app/(authenticated)/main/page.tsx
@@ -1,18 +1,11 @@
 'use client';
 import { useUserContext } from "../context/UserContext";
-import { useEffect } from "react";
 
 export default function MainPage() {
   const { loginUserInfo } = useUserContext();
 
-  useEffect(() => {
-    if (loginUserInfo.app_user_id) {
-      window.location.assign(`/${loginUserInfo.app_user_id}`);
-    }
-  }, [loginUserInfo]);
+  if (loginUserInfo?.app_user_id) {
+    window.location.assign(`/${loginUserInfo.app_user_id}`);
+  }
 
-  return (
-    <div>
-    </div>
-  );
 }
